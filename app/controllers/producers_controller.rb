f5a54c471c2d6417dfe0293e0414de27fe6ce10b
yutaro910 <yayantsukebo@gmail.com>
2021-04-06 03:38:35 +0900

    購入者側の商品一覧機能と商品検索機能と生産者ページへのアクセスの実装

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,10 +1,10 @@
 class ProducersController < ApplicationController
-  before_action :authenticate_producer!
+  before_action :authenticate_producer!, except: :show
 
   def show
     @producer = Producer.find(params[:id])
     @product = @producer.products.page(params[:page]).per(18).order('created_at DESC')
-    unless @producer == current_producer
+    unless current_customer || (@producer == current_producer)
       redirect_to producer_path(current_producer.id)
     end
   end
